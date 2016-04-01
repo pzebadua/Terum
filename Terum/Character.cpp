7e3066dea152923209e19044f1f6084f8d0f9fde
Pablo Zebadúa <pzebadua@MacBook-Pro-de-Pablo-3.local>
2016-03-31 19:53:01 -0600

    Character Class

---
+++ Terum/Character.cpp
@@ -0,0 +1,89 @@
+//
+//  Character.cpp
+//  Terum
+//
+//  Created by Pablo Zebadúa on 31/03/16.
+//  Copyright © 2016 Controversial Games. All rights reserved.
+//
+
+#include "Character.hpp"
+Character::Character()
+{
+    hp = 5;
+    xp = 0;
+    magic = 0;
+    atk = 1;
+
+}
+Character::~Character(void)
+{
+    inventory.removeAll();
+}
+void Character::setLife(int l)
+{
+    hp = l;
+}
+void Character::setExp(int e)
+{
+    xp = e;
+}
+void Character::setMagic(int m)
+{
+    magic = m;
+}
+void Character::setAttack(int a)
+{
+    atk = a;
+}
+
+void Character::setItem(Item i)
+{
+    inventory.insertLast(i);
+}
+void Character::dropItem(int id)
+{
+    for (int i = 0; i < inventory.getSize() ; ++i)
+    {
+        if (inventory[i].value.getID() == id)
+        {
+            inventory.removeAt(i);
+        }
+    }
+
+}
+
+int Character::getLife()
+{
+    return hp;
+}
+int Character::getExp()
+{
+    return xp;
+}
+int Character::getMagic()
+{
+    return magic;
+}
+int Character::getAttack()
+{
+    return atk;
+}
+
+Item Character::getItem(int id)
+{
+    for (int i = 0; i < inventory.getSize() ; ++i)
+    {
+        if (inventory[i].value.getID() == id)
+        {
+            return inventory[i].value;
+        }
+    }
+    Item empty;
+    return empty;
+}
+
+
+LinkedList<Item> Character::getInventory()
+{
+    return inventory;
+}
---
+++ Terum/Character.hpp
@@ -0,0 +1,48 @@
+//
+//  Character.hpp
+//  Terum
+//
+//  Created by Pablo Zebadúa on 31/03/16.
+//  Copyright © 2016 Controversial Games. All rights reserved.
+//
+
+#ifndef Character_hpp
+#define Character_hpp
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include "LinkedList.h"
+#include "Item.hpp"
+using namespace std;
+class Character
+{
+private:
+    LinkedList<Item>inventory;
+    LinkedList<Item>equipped;
+    
+    int hp;
+    int xp;
+    int atk;
+    int magic;
+    
+public:
+    void setLife(int l);
+    int getLife();
+    void setItem(Item i);
+    Item getItem(int id);
+    void setExp(int e);
+    int getExp();
+    void setMagic(int m);
+    int getMagic();
+    void setAttack(int m);
+    int getAttack();
+    LinkedList<Item> getInventory();
+    void dropItem(int id);
+    Character();
+    ~Character(void);
+    //To do: Equip add/remove
+};
+
+
+#endif /* Character_hpp */
